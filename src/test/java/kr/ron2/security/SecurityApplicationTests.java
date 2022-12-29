31ae77ca7d2fcb54746410bc63c646b513038d4b
Minseok-Choi <cms05041@gmail.com>
2022-12-29 22:53:35 +0900

    init: 프로젝트 빌드 및 application.yml 기본 셋팅

---
+++ src/test/java/kr/ron2/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ron2.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
