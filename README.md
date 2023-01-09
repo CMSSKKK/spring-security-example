648657c5b8e856957382deca361c2b5da24ad237
Minseok-Choi <cms05041@gmail.com>
2023-01-09 23:40:43 +0900

    feat: oauth2 설정 및 Adapter를 사용하지않고 설정 추가하기

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 ### References
 
 - https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter
+- https://rlqof7ogm.toastcdn.net/references/2021_session_04.pdf
+- https://www.youtube.com/watch?v=s4X4SJv2RrU
+- https://spring.io/guides/tutorials/spring-boot-oauth2/
--- src/main/java/kr/ron2/security/auth/config/SecurityConfig.java
+++ src/main/java/kr/ron2/security/auth/config/SecurityConfig.java
@@ -1,12 +1,12 @@
 package kr.ron2.security.auth.config;
 
+import kr.ron2.security.auth.CustomOauthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,19 +14,18 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable()
-                .authorizeRequests().anyRequest().permitAll()
+                .authorizeRequests()
+                .anyRequest().permitAll()
                 .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(new DefaultOAuth2UserService())
                 .and()
-                .formLogin().disable()
+                .and()
                 .build();
     }
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+
 }
